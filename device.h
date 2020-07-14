142ff80a31bee62dff044a7c81eb27212eb3ad83
serisman <github@serisman.com>
2020-07-13 21:33:21 -0500

    Sort devices alphabetically

--- device.h
+++ device.h
@@ -1,7 +1,13 @@
 #ifndef __PDK_DEVICE_H__
 #define __PDK_DEVICE_H__
 
-#if defined(PMS131)
+#if defined(PFS154)
+  #include "device/pfs154.h"
+#elif defined(PFS172)
+  #include "device/pfs172.h"
+#elif defined(PFS173)
+  #include "device/pfs173.h"
+#elif defined(PMS131)
   #include "device/pms131.h"
 #elif defined(PMS150C)
   #include "device/pms150c.h"
@@ -15,12 +21,6 @@
   #include "device/pms154c.h"
 #elif defined(PMS171B)
   #include "device/pms171b.h"
-#elif defined(PFS154)
-  #include "device/pfs154.h"
-#elif defined(PFS172)
-  #include "device/pfs172.h"
-#elif defined(PFS173)
-  #include "device/pfs173.h"
 #else
 	#error "Unknown device. Please define device!"
 #endif
